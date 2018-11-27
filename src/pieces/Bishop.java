46370f9af1e41784bcd014f9866dd150542fd2c6
erikwirenhed <erik.wirenhed.te16d@gmail.com>
2018-11-27 14:52:05 +0100

    jag har effikticerat bishop

--- src/pieces/Bishop.java
+++ src/pieces/Bishop.java
@@ -18,90 +18,8 @@ public class Bishop extends Piece {
 	@Override
 	public void showMove() {
 
-		Square s = (Square) this.getParent();
-
-		for (int index = Square.all_squares.indexOf(s) + 7; index < Square.all_squares.size(); index += 7) {
-
-			if (index % 8 == 7) {
-				break;
-			}
-			if (Square.all_squares.get(index).hasPiece()
-					&& !(this.getColor() != Square.all_squares.get(index).getPieceColor())) {
-				break;
-			} else if (Square.all_squares.get(index).hasPiece()
-					&& (this.getColor() != Square.all_squares.get(index).getPieceColor())) {
-				if (Square.all_squares.get(index).hasKing()) {
-					ChessBoard.check();
-				}
-				Square.all_squares.get(index).getBackground().setFill(Color.GREEN);
-				break;
-			} else {
-
-				Square.all_squares.get(index).getBackground().setFill(Color.RED);
-			}
-
-		}
-		for (int index = Square.all_squares.indexOf(s) + 9; index < Square.all_squares.size(); index += 9) {
-			if (index % 8 == 0) {
-				break;
-			}
-			if (Square.all_squares.get(index).hasPiece()
-					&& !(this.getColor() != Square.all_squares.get(index).getPieceColor())) {
-				break;
-			} else if (Square.all_squares.get(index).hasPiece()
-					&& (this.getColor() != Square.all_squares.get(index).getPieceColor())) {
-				if (Square.all_squares.get(index).hasKing()) {
-					ChessBoard.check();
-				}
-				Square.all_squares.get(index).getBackground().setFill(Color.GREEN);
-				break;
-			} else {
-
-				Square.all_squares.get(index).getBackground().setFill(Color.RED);
-			}
-
-		}
-		for (int index = Square.all_squares.indexOf(s) - 7; index >= 0; index -= 7) {
-			if (index % 8 == 0) {
-				break;
-			}
-			if (Square.all_squares.get(index).hasPiece()
-					&& !(this.getColor() != Square.all_squares.get(index).getPieceColor())) {
-				break;
-			} else if (Square.all_squares.get(index).hasPiece()
-					&& (this.getColor() != Square.all_squares.get(index).getPieceColor())) {
-				if (Square.all_squares.get(index).hasKing()) {
-					ChessBoard.check();
-				}
-				Square.all_squares.get(index).getBackground().setFill(Color.GREEN);
-				break;
-			} else {
-
-				Square.all_squares.get(index).getBackground().setFill(Color.RED);
-			}
-
-		}
-		for (int index = Square.all_squares.indexOf(s) - 9; index >= 0; index -= 9) {
-			if (index % 8 == 7) {
-				break;
-			}
-			if (Square.all_squares.get(index).hasPiece()
-					&& !(this.getColor() != Square.all_squares.get(index).getPieceColor())) {
-				break;
-			} else if (Square.all_squares.get(index).hasPiece()
-					&& (this.getColor() != Square.all_squares.get(index).getPieceColor())) {
-				if (Square.all_squares.get(index).hasKing()) {
-					ChessBoard.check();
-				}
-				Square.all_squares.get(index).getBackground().setFill(Color.GREEN);
-				break;
-			} else {
-
-				Square.all_squares.get(index).getBackground().setFill(Color.RED);
-			}
-
-		}
-
+			walk(7);
+			walk(9);
 	}
 
 	@Override
@@ -109,5 +27,66 @@ public class Bishop extends Piece {
 		// TODO Auto-generated method stub
 
 	}
+	
+	private void walk(int x){
+		
+		int c = 0;
+		
+		Square s = (Square) this.getParent();
+
+		for (int index = Square.all_squares.indexOf(s) + x; index < Square.all_squares.size(); index += x) {
+
+			if(x==7){
+				c=x;
+			}
+			
+			
+			if (index % 8 == c) {
+				break;
+			}
+			if (Square.all_squares.get(index).hasPiece()
+					&& !(this.getColor() != Square.all_squares.get(index).getPieceColor())) {
+				break;
+			} else if (Square.all_squares.get(index).hasPiece()
+					&& (this.getColor() != Square.all_squares.get(index).getPieceColor())) {
+				if (Square.all_squares.get(index).hasKing()) {
+					ChessBoard.check();
+				}
+				Square.all_squares.get(index).getBackground().setFill(Color.GREEN);
+				break;
+			} else {
+
+				Square.all_squares.get(index).getBackground().setFill(Color.RED);
+			}
+
+		}
+		
+		for (int index = Square.all_squares.indexOf(s) - x; index >= 0; index -= x) {
+			
+			if(x==9){
+				c=7;
+			}
+			
+			if (index % 8 == c) {
+				break;
+			}
+			if (Square.all_squares.get(index).hasPiece()
+					&& !(this.getColor() != Square.all_squares.get(index).getPieceColor())) {
+				break;
+			} else if (Square.all_squares.get(index).hasPiece()
+					&& (this.getColor() != Square.all_squares.get(index).getPieceColor())) {
+				if (Square.all_squares.get(index).hasKing()) {
+					ChessBoard.check();
+				}
+				Square.all_squares.get(index).getBackground().setFill(Color.GREEN);
+				break;
+			} else {
+
+				Square.all_squares.get(index).getBackground().setFill(Color.RED);
+			}
+
+		}
+	}
+	
 
 }
